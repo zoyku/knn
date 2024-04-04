53a0e3b74359454c3ad896813f8d0fbd6b224bb5
ZEYNEP OYKU BUYUKBAY <e2521318@ceng.metu.edu.tr>
2024-04-04 15:33:31 +0000

    numpy added

--- hw2.py
+++ hw2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def knn(existing_data: pd.DataFrame, test_data: pd.DataFrame, k: int, distance_method: str, re_training: bool, distance_threshold: float, weighted_voting: bool):
     
