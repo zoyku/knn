38366a1111e3aeae3d32e89c76657326e4536f32
ZEYNEP OYKU BUYUKBAY <e2521318@ceng.metu.edu.tr>
2024-04-04 15:33:09 +0000

    read csv

--- hw2.py
+++ hw2.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 def knn(existing_data: pd.DataFrame, test_data: pd.DataFrame, k: int, distance_method: str, re_training: bool, distance_threshold: float, weighted_voting: bool):
+    
+    string_data = pd.read_csv(existing_data)
+    string_data = pd.read_csv(test_data)
+
     return 0
